7eff08753a5a4cabe3a022323d9b6f70fbac99be
Exeldro <exelsor@hotmail.com>
2018-11-10 13:12:57 +0100

    update match rules mapping

--- Ochs/NHibernate/MatchRulesMap.cs
+++ Ochs/NHibernate/MatchRulesMap.cs
@@ -13,6 +13,19 @@ namespace Ochs
             Map(x => x.ExchangePointsMax);
             Map(x => x.TimeMax);
             Map(x => x.ExchangesMax);
+            Map(x => x.DoubleHitMax);
+            Map(x => x.BothCanScore);
+            Map(x => x.SubtractPoints);
+            Map(x => x.RecordSportsmanship);
+            Map(x => x.RecordWarnings);
+            Map(x => x.RecordProtests);
+            Map(x => x.RecordAfterblow);
+            Map(x => x.AfterblowScores);
+            Map(x => x.RecordDoubleHits);
+            Map(x => x.DoubleHitScores);
+            Map(x => x.RecordUnclearExchanges);
+            Map(x => x.RecordPenalties);
+            Map(x => x.PenaltyPoints);
         }
     }
 }
